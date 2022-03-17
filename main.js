0400e6103a342e5aa2a6e092b010e73186ccd47c
SheldonHughes <sheldondhughes#gmail.com>
2022-03-17 17:40:30 -0500

    Fixed users not populating

--- main.js
+++ main.js
@@ -4,26 +4,28 @@ fetch('phone_numbers.json')
   .then(response => response.json())
   .then(json => {
     // console.log(json)
-    // for(let user in json) {
-    //   // console.log(user, json[user])
-    //   for(let el in json[user]) {
-    //     console.log(user, json[user].Company)
-    //   }
-    // }
+
     //Check into removing the first json.map and beginning with Object.entries()
-    users = //json.map(user => {
-      Object.entries(json).map(([key, value])=>{
-        console.log(`This is ${key} of ${value.Company}`)
+   json.map(user => {
+      // console.log(user)
+      // debugger
+      Object.entries(user).forEach(([key, value])=>{
+        // console.log(`This is ${key} of ${value.Company}`)
         let box = document.createElement("div")
         box.className = "box"
+        //TODO: Add phone number formatting
         box.innerHTML = `Name: ${key}<br>
-        ${value.Company}`
+                        Company: ${value.Company}<br>
+                        
+                        Phone#: ${value.Phone}`
         document.getElementById('container').appendChild(box)
-        return {company: value.Company, name: key, box: box}
+        users.push({company: value.Company, name: key, phone: value.Phone, box: box})
       })
             })
-       //   }
-       //)
+        }
+      )
+      
+// console.log(users)
 
 const searchItem = document.querySelector('.searchbox')
   searchItem.addEventListener('input', e =>{
