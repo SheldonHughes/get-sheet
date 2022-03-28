a9e288f6e963a61e7614e4783bc7857abc3e7333
SheldonHughes <sheldondhughes#gmail.com>
2022-03-28 16:23:38 -0500

    very minor tweaks

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 import { users } from "./source/source"
-// console.log(users)
+console.log(users)
 
 const searchItem = document.querySelector('.searchbox')
   searchItem.addEventListener('input', e =>{
     const value = isNaN(e.target.value) ? e.target.value.toString().toLowerCase() : e.target.value.toLowerCase()
     users.forEach(user =>{
-      // console.log(user)
+      console.log(user)
       const isVisible = user.company.toLowerCase().includes(value) || user.name.toLowerCase().includes(value)
       user.box.classList.toggle('hide', !isVisible)
     })
@@ -17,16 +17,19 @@ console.log(companyNames);
 const section = document.getElementById('company-list')
 
 
-function getUniqueNames(){
+async function getUniqueNames(){
   users.forEach(user =>{
     const companyName = user.company
+    console.log(companyName)
     if(!companyNames.includes(companyName))
       companyNames.push(companyName)
     })
+    // return Promise.resolve()
   }
 
 
 async function listCompanyNames() {
+  console.log('called')
   await getUniqueNames();
   companyNames.forEach(name =>{
   console.log(name)
