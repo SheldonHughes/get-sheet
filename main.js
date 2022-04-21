846bb67571d3d2b2947fa684c0a1737f8eac5592
Sheldon Hughes <sheldondhughes@gmail.com>
2022-04-21 08:25:19 -0500

    display:hide not working

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 import { users } from "./source/source"
 // console.log(users)
+const section = document.getElementById('company-list')
 
 const searchItem = document.querySelector('.searchbox')
   searchItem.addEventListener('input', e =>{
     const value = isNaN(e.target.value) ? e.target.value.toString().toLowerCase() : e.target.value.toLowerCase()
+    console.log(value)
     users.forEach(user =>{
-      console.log(user)
+      // console.log(user)
       const isVisible = user.company.toLowerCase().includes(value) || user.name.toLowerCase().includes(value)
+      // console.log(isVisible)
       user.box.classList.toggle('hide', !isVisible)
     })
 })
@@ -14,13 +17,23 @@ const searchItem = document.querySelector('.searchbox')
 let companyNames = []
 console.log(companyNames);
 
-const section = document.getElementById('company-list')
+function renderCompanies(company) {
+  console.log(`${company} was clicked.`)
+  // let box = document.createElement("div")
+  //       box.className = "box"
+  //       //TODO: Add phone number formatting
+  //       box.innerHTML = `Name: ${key}<br>
+  //                       Company: ${value.Company}<br>
+                        
+  //                       Phone#: ${value.Phone}`
+  //       document.getElementById('container').appendChild(box)
+}
 
 
 async function getUniqueNames(){
   users.forEach(user =>{
     const companyName = user.company
-    // console.log(companyName)
+    console.log(companyName)
     if(!companyNames.includes(companyName))
       companyNames.push(companyName)
     })
@@ -31,11 +44,13 @@ async function listCompanyNames() {
   console.log('called')
   await getUniqueNames();
   companyNames.forEach(name =>{
-  console.log(name)
+  // console.log(name)
     let list = document.createElement('li')
     list.classList.add('company')
        list.insertAdjacentHTML("beforeend", `<a href="javascript:void(0);">${name}<span class="material-icons md-18">group</span></a>`)
     section.appendChild(list)
+    list.addEventListener('click', function(){
+      renderCompanies(name)})
   })
 }
 
--- source/source.js
+++ source/source.js
@@ -4,14 +4,8 @@ export let users = [];
 fetch('phone_numbers.json')
   .then(response => response.json())
   .then(json => {
-    // console.log(json)
-
-    //Check into removing the first json.map and beginning with Object.entries()
    json.map(user => {
-      // console.log(user)
-      // debugger
       Object.entries(user).forEach(([key, value])=>{
-        // console.log(`This is ${key} of ${value.Company}`)
         let box = document.createElement("div")
         box.className = "box"
         //TODO: Add phone number formatting
